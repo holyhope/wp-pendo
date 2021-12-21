f8c99ffb8195614e70678c78b74d816913d23adc
Pierre PÉRONNET <pierre.peronnet@gmail.com>
2021-12-21 10:20:38 +0100

    fix(settings) Use wppendo prefix

--- settings.php
+++ settings.php
@@ -7,37 +7,37 @@
 /**
  * custom option and settings
  */
-function wporg_settings_init() {
-    // Register a new setting for "wporg" page.
-    register_setting( 'wporg', 'wporg_options' );
+function wppendo_settings_init() {
+    // Register a new setting for "wppendo" page.
+    register_setting( 'wppendo', 'wppendo_options' );
  
-    // Register a new section in the "wporg" page.
+    // Register a new section in the "wppendo" page.
     add_settings_section(
-        'wporg_section_developers',
-        __( 'The Matrix has you.', 'wporg' ), 'wporg_section_developers_callback',
-        'wporg'
+        'wppendo_section_developers',
+        __( 'The Matrix has you.', 'wppendo' ), 'wppendo_section_developers_callback',
+        'wppendo'
     );
  
-    // Register a new field in the "wporg_section_developers" section, inside the "wporg" page.
+    // Register a new field in the "wppendo_section_developers" section, inside the "wppendo" page.
     add_settings_field(
-        'wporg_field_pill', // As of WP 4.6 this value is used only internally.
+        'wppendo_field_pill', // As of WP 4.6 this value is used only internally.
                                 // Use $args' label_for to populate the id inside the callback.
-            __( 'Pill', 'wporg' ),
-        'wporg_field_pill_cb',
-        'wporg',
-        'wporg_section_developers',
+            __( 'Pill', 'wppendo' ),
+        'wppendo_field_pill_cb',
+        'wppendo',
+        'wppendo_section_developers',
         array(
-            'label_for'         => 'wporg_field_pill',
-            'class'             => 'wporg_row',
-            'wporg_custom_data' => 'custom',
+            'label_for'         => 'wppendo_field_pill',
+            'class'             => 'wppendo_row',
+            'wppendo_custom_data' => 'custom',
         )
     );
 }
  
 /**
- * Register our wporg_settings_init to the admin_init action hook.
+ * Register our wppendo_settings_init to the admin_init action hook.
  */
-add_action( 'admin_init', 'wporg_settings_init' );
+add_action( 'admin_init', 'wppendo_settings_init' );
  
  
 /**
@@ -51,9 +51,9 @@ add_action( 'admin_init', 'wporg_settings_init' );
  *
  * @param array $args  The settings array, defining title, id, callback.
  */
-function wporg_section_developers_callback( $args ) {
+function wppendo_section_developers_callback( $args ) {
     ?>
-    <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Follow the white rabbit.', 'wporg' ); ?></p>
+    <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Follow the white rabbit.', 'wppendo' ); ?></p>
     <?php
 }
  
@@ -67,26 +67,26 @@ function wporg_section_developers_callback( $args ) {
  *
  * @param array $args
  */
-function wporg_field_pill_cb( $args ) {
+function wppendo_field_pill_cb( $args ) {
     // Get the value of the setting we've registered with register_setting()
-    $options = get_option( 'wporg_options' );
+    $options = get_option( 'wppendo_options' );
     ?>
     <select
             id="<?php echo esc_attr( $args['label_for'] ); ?>"
-            data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
-            name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
+            data-custom="<?php echo esc_attr( $args['wppendo_custom_data'] ); ?>"
+            name="wppendo_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
         <option value="red" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'red', false ) ) : ( '' ); ?>>
-            <?php esc_html_e( 'red pill', 'wporg' ); ?>
+            <?php esc_html_e( 'red pill', 'wppendo' ); ?>
         </option>
         <option value="blue" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'blue', false ) ) : ( '' ); ?>>
-            <?php esc_html_e( 'blue pill', 'wporg' ); ?>
+            <?php esc_html_e( 'blue pill', 'wppendo' ); ?>
         </option>
     </select>
     <p class="description">
-        <?php esc_html_e( 'You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.', 'wporg' ); ?>
+        <?php esc_html_e( 'You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.', 'wppendo' ); ?>
     </p>
     <p class="description">
-        <?php esc_html_e( 'You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'wporg' ); ?>
+        <?php esc_html_e( 'You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'wppendo' ); ?>
     </p>
     <?php
 }
@@ -94,27 +94,27 @@ function wporg_field_pill_cb( $args ) {
 /**
  * Add the top level menu page.
  */
-function wporg_options_page() {
+function wppendo_options_page() {
     add_menu_page(
-        'WPOrg',
-        'WPOrg Options',
+        'wppendo',
+        'wppendo Options',
         'manage_options',
-        'wporg',
-        'wporg_options_page_html'
+        'wppendo',
+        'wppendo_options_page_html'
     );
 }
  
  
 /**
- * Register our wporg_options_page to the admin_menu action hook.
+ * Register our wppendo_options_page to the admin_menu action hook.
  */
-add_action( 'admin_menu', 'wporg_options_page' );
+add_action( 'admin_menu', 'wppendo_options_page' );
  
  
 /**
  * Top level menu callback function
  */
-function wporg_options_page_html() {
+function wppendo_options_page_html() {
     // check user capabilities
     if ( ! current_user_can( 'manage_options' ) ) {
         return;
@@ -126,21 +126,21 @@ function wporg_options_page_html() {
     // WordPress will add the "settings-updated" $_GET parameter to the url
     if ( isset( $_GET['settings-updated'] ) ) {
         // add settings saved message with the class of "updated"
-        add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
+        add_settings_error( 'wppendo_messages', 'wppendo_message', __( 'Settings Saved', 'wppendo' ), 'updated' );
     }
  
     // show error/update messages
-    settings_errors( 'wporg_messages' );
+    settings_errors( 'wppendo_messages' );
     ?>
     <div class="wrap">
         <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
         <form action="options.php" method="post">
             <?php
-            // output security fields for the registered setting "wporg"
-            settings_fields( 'wporg' );
+            // output security fields for the registered setting "wppendo"
+            settings_fields( 'wppendo' );
             // output setting sections and their fields
-            // (sections are registered for "wporg", each field is registered to a specific section)
-            do_settings_sections( 'wporg' );
+            // (sections are registered for "wppendo", each field is registered to a specific section)
+            do_settings_sections( 'wppendo' );
             // output save settings button
             submit_button( 'Save Settings' );
             ?>
