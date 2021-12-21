18c46627a3f0f708266de71dcc1edb149a29b5c0
Pierre PÉRONNET <pierre.peronnet@gmail.com>
2021-12-21 10:10:27 +0100

    $ wp scaffold plugin wp-pendo --plugin_name="WP Pendo" --plugin_description="Enable pendo.io on WordPress" --plugin_author="Pierre PÉRONNET"

---
+++ wp-pendo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     WP Pendo
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Enable pendo.io on WordPress
+ * Author:          Pierre PÉRONNET
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-pendo
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Pendo
+ */
